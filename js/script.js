abcce1d68f70410897f55860710ac977627c14a6
Christian Moore <moorechristian15@yahoo.com>
2025-01-07 16:52:24 -0500

    Updated the html, css, and js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
-//selecting html elements
-//attaching 'click listener'
-//getting user entered values 
-//javascript validations
-
 let emailElement = document.querySelector("#email");
 let messageElement = document.querySelector("#message");
 let submitButton = document.querySelector("#submit-button");
 
+
 submitButton.addEventListener("click", function(e) {
     e.preventDefault();
-
+    
     let emailValue = emailElement.value;
     let messageValue = messageElement.value;
-
-    if (emailValue.includes('@')) {
-        //all good
-        alert("Email is valid. Thank you for your message");
-    }
-    else {
+    
+    if (!emailValue.includes('@')) {
         alert("Email is not valid. Please enter a valid email address");
     }
+    else{
+        alert("Email function coming soon!");
+    }
 })
+
+document.getElementById("submit-button").addEventListener("click", function () {
+    alert("Email function coming soon!");
+});
+
+// Function to validate the email form
+function validateForm() {
+  const email = document.getElementById("email");
+  if (!email.value.includes("@")) {
+      email.style.borderColor = "red";
+      return false;
+  }
+  return true;
+}
+
 
 // function to redirect to another page. Usually used after form submission.
 function redirectToPage(url) {
@@ -48,24 +58,3 @@ function toggleMenu() {
     const nav = document.getElementById("navMenu");
     nav.classList.toggle('active');
 }
-
-  // Function to validate the email form
-function validateForm() {
-    const email = document.getElementById("email");
-    if (!email.value.includes("@")) {
-        email.style.borderColor = "red";
-        return false;
-    }
-    return true;
-}
-  
-/*
-document.getElementById("header").textContent = "Welcome to My Portfolio!";
-document.querySelector(".highlight").style.color = "blue";
-document.getElementById("submit-btn").addEventListener("click", function () {
-    alert("Form submitted!");
-});
-*/
-
-//alert("Hello, World!")
-
